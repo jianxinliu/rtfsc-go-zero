b7d2c6b15e1dfebf580c44323c82c19da4452dd9
jianxinliu <jianxinliu@wumitech.com>
2023-06-15 21:20:32 +0800

    singleflight & limit

--- core/syncx/limit.go
+++ core/syncx/limit.go
@@ -6,6 +6,14 @@ import (
 	"github.com/zeromicro/go-zero/core/lang"
 )
 
+/**
+ * [rtfsc]
+ * 主题: limit.go
+ * 摘要: 类似信号量
+ * 功能: 用于控制数量，如并发数
+ * [end]
+ */
+
 // ErrLimitReturn indicates that the more than borrowed elements were returned.
 var ErrLimitReturn = errors.New("discarding limited token, resource pool is full, someone returned multiple times")
 
@@ -14,6 +22,9 @@ type Limit struct {
 	pool chan lang.PlaceholderType
 }
 
+// [rtfsc]
+// 创建一个限制器，并规定总的数量
+// [end]
 // NewLimit creates a Limit that can borrow n elements from it concurrently.
 func NewLimit(n int) Limit {
 	return Limit{
@@ -21,11 +32,19 @@ func NewLimit(n int) Limit {
 	}
 }
 
+// [rtfsc]
+// 占用一个信号量，通过向 channel 写入实现。
+// 利用 channel 的特性，channel 满了之后再写入，就得等待
+// 以此来实现限制的作用
+// [end]
 // Borrow borrows an element from Limit in blocking mode.
 func (l Limit) Borrow() {
 	l.pool <- lang.Placeholder
 }
 
+// [rtfsc]
+// 归还信号量
+// [end]
 // Return returns the borrowed resource, returns error only if returned more than borrowed.
 func (l Limit) Return() error {
 	select {
@@ -36,6 +55,9 @@ func (l Limit) Return() error {
 	}
 }
 
+// [rtfsc]
+// 使用非阻塞的方式尝试获取一个信号量，获取不到则返回 false
+// [end]
 // TryBorrow tries to borrow an element from Limit, in non-blocking mode.
 // If success, true returned, false for otherwise.
 func (l Limit) TryBorrow() bool {
--- core/syncx/singleflight.go
+++ core/syncx/singleflight.go
@@ -2,6 +2,15 @@ package syncx
 
 import "sync"
 
+/**
+ * [rtfsc]
+ * 主题: singleflight.go
+ * 摘要: 相同的任务，只需要一个人执行完成，剩下的享受成果即可
+ * 功能: 多个协程执行同一个任务时，只需要一个执行成功，其余的共享结果即可
+ * 应用: 高并发查询某个缓存未命中的记录，则只需允许一个协程去 db 查询即可，再将结果存到缓存，其余的直接获取缓存即可
+ * [end]
+ */
+
 type (
 	// SingleFlight lets the concurrent calls with the same key to share the call result.
 	// For example, A called F, before it's done, B called F. Then B would not execute F,
@@ -14,12 +23,23 @@ type (
 		DoEx(key string, fn func() (any, error)) (any, bool, error)
 	}
 
+	// [rtfsc]
+	// 表示一次任务
+	// wg  用于等待任务执行结束
+	// val 任务执行的返回值
+	// err 任务执行的错误
+	// [end]
 	call struct {
 		wg  sync.WaitGroup
 		val any
 		err error
 	}
 
+	// [rtfsc]
+	// 管理所有任务
+	// calls 按 key 存放任务。实际上只存放一个执行中的任务，其他相同 key 的任务进来后，会直接返回已经存在的任务的引用
+	// lock  用于在增减任务时加锁，毕竟 map 不是并发安全的
+	// [end]
 	flightGroup struct {
 		calls map[string]*call
 		lock  sync.Mutex
@@ -33,6 +53,12 @@ func NewSingleFlight() SingleFlight {
 	}
 }
 
+// [rtfsc]
+// 执行一个任务
+// 返回任务执行的结果。结果来自于：
+// 1. 首次触发，调用 fn 得到
+// 2. 非首次触发，共享首次触发时的结果
+// [end]
 func (g *flightGroup) Do(key string, fn func() (any, error)) (any, error) {
 	c, done := g.createCall(key)
 	if done {
@@ -43,6 +69,10 @@ func (g *flightGroup) Do(key string, fn func() (any, error)) (any, error) {
 	return c.val, c.err
 }
 
+// [rtfsc]
+// 执行一个任务，同 Do
+// 不同之处在于，会返回的结果是由本次触发得到的，还是由其他触发得到的 (fresh：新鲜的结果)
+// [end]
 func (g *flightGroup) DoEx(key string, fn func() (any, error)) (val any, fresh bool, err error) {
 	c, done := g.createCall(key)
 	if done {
@@ -53,6 +83,16 @@ func (g *flightGroup) DoEx(key string, fn func() (any, error)) (val any, fresh b
 	return c.val, true, c.err
 }
 
+// [rtfsc]
+// 创建或者获取一个已经存在的 call，返回其引用
+// c    call 的引用
+// done 本次 call 是否完成了任务。完成了，则可以直接取 c.val
+//
+// 过程：
+// 1. 操作 map 时加锁
+// 2. 看当前的 key 是否有 call 在执行，有则直接取出，并等待这个 call 执行结束后返回。这样，同一个 key 不管调用多少次，都会等待同一个 call 的返回结果
+// 3. 没有，则创建一个 call，并加入 map
+// [end]
 func (g *flightGroup) createCall(key string) (c *call, done bool) {
 	g.lock.Lock()
 	if c, ok := g.calls[key]; ok {
@@ -69,6 +109,9 @@ func (g *flightGroup) createCall(key string) (c *call, done bool) {
 	return c, false
 }
 
+// [rtfsc]
+// 实际执行 call, 执行完成后，删除 map 中的 call
+// [end]
 func (g *flightGroup) makeCall(c *call, key string, fn func() (any, error)) {
 	defer func() {
 		g.lock.Lock()
